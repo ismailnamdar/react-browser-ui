ff4fcc5f38937f2aced4ea15be72d552b4b6d352
İsmail Namdar <namdari@itu.edu.tr>
2020-05-06 23:27:21 +0300

    fix(app): change example tab content

--- example/src/App.js
+++ example/src/App.js
@@ -1,30 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import Browser, { Tab, Divider, AddButton } from 'react-browser-ui'
 
-export default class App extends Component {
-  render () {
-    const tabEnd = (
-      <>
-        <Divider />
-        <AddButton />
-      </>
-    );
-    return (
-      <div style={{ width: '100%', height: 600 }}>
-        <Browser
-          type={'chrome'}
-          showHeader={true}
-          activeTabKey={'green'}
-          tabEnd={tabEnd}>
-          <Tab key={'green'} title={'Green'}>
-            <div style={{ backgroundColor: 'green', height: '100%', width: '100%', opacity: 0.9 }} />
-          </Tab>
-          <Tab key={'blue'} title={'Blue'}>
-            <div style={{ backgroundColor: 'blue', height: '100%', width: '100%', opacity: 0.9 }} />
-          </Tab>
-        </Browser>
-      </div>
-    )
-  }
+export default function App () {
+  const tabEnd = (
+    <>
+      <Divider />
+      <AddButton />
+    </>
+  )
+  return (
+    <div style={{ width: 600, height: 500, padding: '5em' }}>
+      <Browser
+        type={'chrome'}
+        showHeader={false}
+        activeTabKey={'green'}
+        tabEnd={tabEnd}>
+        <Tab key={'green'} title={'Green'}>
+          <div style={{ backgroundColor: 'green', height: '100%', width: '100%', opacity: 0.9, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <h1 style={{ color: 'white', margin: 0 }}>{'Your component here'}</h1>
+          </div>
+        </Tab>
+        <Tab key={'blue'} title={'Blue'}>
+          <div style={{ backgroundColor: 'green', height: '100%', width: '100%', opacity: 0.9, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <h1 style={{ color: 'white', margin: 0 }}>{'Your component here'}</h1>
+          </div>
+        </Tab>
+      </Browser>
+    </div>
+  )
 }
