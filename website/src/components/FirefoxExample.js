7ceb6194db46bc057970c4038d62a962336cc034
İsmail Namdar <namdari@itu.edu.tr>
2020-05-09 20:00:26 +0300

    fix(website): change background color of tabs

--- website/src/components/FirefoxExample.js
+++ website/src/components/FirefoxExample.js
@@ -23,7 +23,7 @@ export default function FirefoxExample () {
           </div>
         </Tab>
         <Tab key={'blue'} imageUrl={''} imageAlt={'blue tab image'} title={'Blue'}>
-          <div style={{ backgroundColor: 'green', height: '100%', width: '100%', opacity: 0.9, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <div style={{ backgroundColor: 'blue', height: '100%', width: '100%', opacity: 0.9, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             <h1 style={{ color: 'white', margin: 0 }}>{'Your component here'}</h1>
           </div>
         </Tab>
